6cf97f46db173dfe3f9fd5981a2cac45e4ad4184
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-01-01 14:57:25 -0500

    fix(db): set cascade deletion of recordson al models

---
+++ server/prisma/migrations/20260101195607_cascade_deletion/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "comment" DROP CONSTRAINT "comment_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comment" DROP CONSTRAINT "comment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "post" DROP CONSTRAINT "post_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "post" ADD CONSTRAINT "post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
