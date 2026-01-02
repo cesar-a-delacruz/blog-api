c826ef97a52d0d2ea4bc7d9d3843a84d563791df
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-01-01 21:49:24 -0500

    feat(client): adapt Post view to add comments

--- client/src/pages/comment/dataFields.js
+++ client/src/pages/comment/dataFields.js
@@ -1,9 +1,8 @@
 export default [
-  { name: "id", type: "hidden" },
+  { name: "postId", type: "hidden" },
   {
     name: "content",
     label: "Content",
     type: "textarea",
   },
-  { name: "postId", type: "hidden" },
 ];
--- client/src/pages/post/View.jsx
+++ client/src/pages/post/View.jsx
@@ -3,15 +3,46 @@ import Post from "@/components/Post";
 import Comment from "@/components/Comment";
 import { useData } from "@/hooks/useData";
 import { useTitle } from "@/hooks/useTitle";
+import sessionHandler from "@/utils/sessionHandler";
+import dataFields from "../comment/dataFields";
+import CustomDialog from "@/components/CustomDialog";
+import CustomForm from "@/components/CustomForm";
+import { useRef } from "react";
+import requestHandler from "@/utils/requestHandler";
 
-export default function Index() {
-  useTitle("Profile");
+export default function View() {
+  useTitle("Post");
   const { id } = useParams();
   const { data } = useData(`post/${id}`);
+
+  const newDialog = useRef(null);
+  const userData = sessionHandler.user();
+
+  const createAction = async (formData) => {
+    formData.postId = id;
+    formData.userId = userData.id;
+    const errors = await requestHandler.post(formData, "comment");
+    if (errors) return errors;
+    location.replace(`/post/${id}`);
+  };
 
   return data ? (
     <div>
       <Post key={data.id} data={data} />
+      {userData.role === "READER" && (
+        <>
+          <button onClick={() => (newDialog.current.open = true)}>
+            Add Comment
+          </button>
+          <CustomDialog ref={newDialog}>
+            <CustomForm
+              fields={dataFields}
+              actionText={"Comment"}
+              actionHandler={createAction}
+            />
+          </CustomDialog>
+        </>
+      )}
       {data.comments.map((comment) => (
         <Comment key={comment.id} data={comment}>
           <b>{comment.user.username}</b>{" "}
